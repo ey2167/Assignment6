f60533e71a34b98dd179531a74a1af25397092f2
ey2167 <fzreo338@gmail.com>
2014-11-19 14:48:02 -0800

    assignment 6 v2

--- ProjectWeek/src/Day.java
+++ ProjectWeek/src/Day.java
@@ -7,64 +7,61 @@
 import java.util.Scanner;
 public class Day {
 		public static void main (String[]args){
-			System.out.println("what day of the week is it?(1-7, sun-sat)");
+			System.out.println("what day of the week is it?(0-6, sun-sat)");
 		Scanner keyboard = new Scanner(System.in);
 		//type in a day of the week
 		int x = keyboard.nextInt(); 
 		// X is the placeholder for the current day
 		
 		
-		// have what day that is chosen be represented by integers 1-7
-	if ( x == 1){
+		// have what day that is chosen be represented by integers 0-6
+	if ( x == 0){
 		System.out.println("It is sunday");
 	}
-	if (x == 2) {
+	if (x == 1) {
 		System.out.println("It is monday");
 	}
-	if (x == 3){
+	if (x == 2){
 		System.out.println("It is tuesday");
 	}
-	if (x == 4) {
+	if (x == 3) {
 		System.out.println("It is Wednesday");
 	}
-	if (x == 5) {
+	if (x == 4) {
 		System.out.println("It is Thursday");
 	}
-	if (x == 6){
+	if (x == 5){
 		System.out.println("It is Friday");
 	}
-	if (x == 7){
+	if (x == 6){
 		System.out.println("It is Saturday");
 	}
 	//prompt user for how many days they wish to go
 	// the x value will hold the day that was listed previously while y value will place the amount of days prompted by the user
 	// the z value will have set the y value's remainder by 7 so that the range of days still there
 	
-	System.out.println ("Fast forward how many days?");
+	System.out.println ("Fast forward/backward how many days?");
 	int y = x + keyboard.nextInt();
 	int z = y % 7 ;
 	
+	System.out.println("Int x= " + x + " int y= " + y + " int z= " + z);
 	//now the z integer will account for the 7 days of the week through its remainder,
-	if ( z == 1){
+	if ( z == 0){
 		System.out.println("It is sunday");
-	}
-	if (z == 2) {
+	}else if (z == 1) {
 		System.out.println("It is monday");
-	}
-	if (y == 3){
+	}else if (z == 2){
 		System.out.println("It is tuesday");
-	}
-	if (y == 4) {
+	}else if (z == 3) {
 		System.out.println("It is Wednesday");
-	}
-	if (y == 5) {
+	}else if (z == 4) {
 		System.out.println("It is Thursday");
-	}
-	if (y == 6){
+	}else if (z == 5){
 		System.out.println("It is Friday");
-	}
-	if (y == 7){
+	}else if (z == 6){
 		System.out.println("It is Saturday");
+	}else{
+		System.out.println("Something went wrong");
 	}
 	
 	

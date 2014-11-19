4f4e66be9b6e07493c58e82c0618134fa9721f8c
ey2167 <fzreo338@gmail.com>
2014-11-19 14:55:15 -0800

    assignment 6 with java doc

--- ProjectWeek/src/Day.java
+++ ProjectWeek/src/Day.java
@@ -9,12 +9,18 @@ public class Day {
 		public static void main (String[]args){
 			System.out.println("what day of the week is it?(0-6, sun-sat)");
 		Scanner keyboard = new Scanner(System.in);
-		//type in a day of the week
+		/*
+		 * type in a day of the week
+		 */
 		int x = keyboard.nextInt(); 
-		// X is the placeholder for the current day
+		/*
+		 *  X is the placeholder for the current day
+		 */
 		
 		
-		// have what day that is chosen be represented by integers 0-6
+		/*
+		 *  have what day that is chosen be represented by integers 0-6
+		 */
 	if ( x == 0){
 		System.out.println("It is sunday");
 	}
@@ -36,16 +42,24 @@ public class Day {
 	if (x == 6){
 		System.out.println("It is Saturday");
 	}
-	//prompt user for how many days they wish to go
-	// the x value will hold the day that was listed previously while y value will place the amount of days prompted by the user
-	// the z value will have set the y value's remainder by 7 so that the range of days still there
+	/*
+	 * prompt user for how many days they wish to go
+	 */
+	/*
+	 *  the x value will hold the day that was listed previously while y value will place the amount of days prompted by the user
+	 */
+	/*
+	 *  the z value will have set the y value's remainder by 7 so that the range of days still there
+	 */
 	
 	System.out.println ("Fast forward/backward how many days?");
 	int y = x + keyboard.nextInt();
 	int z = y % 7 ;
 	
 	System.out.println("Int x= " + x + " int y= " + y + " int z= " + z);
-	//now the z integer will account for the 7 days of the week through its remainder,
+	/*
+	 * now the z integer will account for the 7 days of the week through its remainder,
+	 */
 	if ( z == 0){
 		System.out.println("It is sunday");
 	}else if (z == 1) {
